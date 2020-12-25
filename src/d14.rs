afa23d89925cdefb7a9faeb8234b917b819eb176
j-zehnder <20309337+j-zehnder@users.noreply.github.com>
2020-12-25 11:23:37 -0500

    d14

---
+++ src/d14.rs
@@ -0,0 +1,141 @@
+use fancy_regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Instruction {
+    Mask(String),
+    Mem(usize, usize),
+}
+
+impl Instruction {
+    fn from_str(s: &str) -> Self {
+        let re_mask = Regex::new(r"^mask = (.{36})$").unwrap();
+        let re_mem = Regex::new(r"^mem\[(\d*)] = (\d*)$").unwrap();
+
+        if re_mask.is_match(s).unwrap() {
+            let captures = re_mask.captures(s).unwrap().unwrap();
+            let mask = captures.get(1).unwrap().as_str().to_string();
+            return Instruction::Mask(mask);
+        } else if re_mem.is_match(s).unwrap() {
+            let captures = re_mem.captures(s).unwrap().unwrap();
+            let index = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let val = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+            return Instruction::Mem(index, val);
+        }
+        panic!("invalid format")
+    }
+}
+
+struct CPU {
+    mask: String,
+    mem: HashMap<usize, usize>,
+}
+
+impl CPU {
+    fn new() -> Self {
+        CPU {
+            mask: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX".to_string(),
+            mem: HashMap::new(),
+        }
+    }
+
+    fn process(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::Mask(mask) => self.mask = mask.clone(),
+            Instruction::Mem(i, v) => {
+                // apply mask
+                let vstr = format!("{:036b}", v);
+
+                let mut val_string = String::new();
+                self.mask
+                    .chars()
+                    .into_iter()
+                    .zip(vstr.chars().into_iter())
+                    .for_each(|i| match i.0 {
+                        'X' => val_string = format!("{}{}", val_string, i.1),
+                        '1' => val_string = format!("{}{}", val_string, 1),
+                        '0' => val_string = format!("{}{}", val_string, 0),
+                        _ => panic!("invalid mask"),
+                    });
+                self.mem
+                    .insert(*i, usize::from_str_radix(val_string.as_str(), 2).unwrap());
+            }
+        }
+    }
+
+    fn process2(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::Mask(mask) => self.mask = mask.clone(),
+            Instruction::Mem(mem, value) => {
+                for mem_address in apply_mask(self.mask.clone(), *mem) {
+                    self.mem.insert(mem_address, *value);
+                }
+            }
+        }
+    }
+}
+
+fn apply_mask(mask: String, mem: usize) -> Vec<usize> {
+    let memstr = format!("{:036b}", mem);
+
+    let mut addresses: Vec<String> = Vec::new();
+    addresses.push("".to_string());
+
+    mask.chars()
+        .into_iter()
+        .zip(memstr.chars().into_iter())
+        .for_each(|i| match i.0 {
+            '0' => {
+                let mut new_addresses: Vec<String> = Vec::new();
+                for a in &addresses {
+                    let new_a = format!("{}{}", a, i.1);
+                    new_addresses.push(new_a);
+                }
+                addresses = new_addresses;
+            }
+            '1' => {
+                let mut new_addresses: Vec<String> = Vec::new();
+                for a in &addresses {
+                    let new_a = format!("{}{}", a, 1);
+                    new_addresses.push(new_a);
+                }
+                addresses = new_addresses;
+            }
+            'X' => {
+                let mut new_addresses: Vec<String> = Vec::new();
+                for a in &addresses {
+                    let new_a_0 = format!("{}{}", a, 0);
+                    let new_a_1 = format!("{}{}", a, 1);
+                    new_addresses.push(new_a_0);
+                    new_addresses.push(new_a_1);
+                }
+                addresses = new_addresses;
+            }
+            _ => panic!("unknown mask"),
+        });
+
+    let mut memory_addresses: Vec<usize> = Vec::new();
+    for mem in addresses {
+        memory_addresses.push(usize::from_str_radix(mem.as_str(), 2).unwrap());
+    }
+    memory_addresses
+}
+
+#[aoc_generator(day14)]
+pub fn parse_input(input: &str) -> Vec<Instruction> {
+    input.lines().map(Instruction::from_str).collect()
+}
+
+#[aoc(day14, part1)]
+pub fn part1(instructions: &[Instruction]) -> usize {
+    let mut cpu = CPU::new();
+    instructions.iter().for_each(|i| cpu.process(i));
+    cpu.mem.values().sum()
+}
+
+#[aoc(day14, part2)]
+pub fn part2(instructions: &[Instruction]) -> usize {
+    let mut cpu = CPU::new();
+    instructions.iter().for_each(|i| cpu.process2(i));
+    cpu.mem.values().sum()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,30 @@ extern crate aoc_runner_derive;
 #[macro_use]
 extern crate scan_fmt;
 
-pub mod d01;
-pub mod d02;
-pub mod d03;
-pub mod d04;
-pub mod d05;
-pub mod d06;
-pub mod d07;
-pub mod d08;
-pub mod d09;
-pub mod d10;
-// pub mod d11;
-pub mod d12;
-// pub mod d13;
-// pub mod d14;
-pub mod d15;
-// pub mod d16;
-// pub mod d17;
-// pub mod d18;
-// pub mod d19;
-// pub mod d20;
-// pub mod d21;
-// pub mod d22;
-// pub mod d23;
-pub mod d24;
+pub mod d01;  // 2 and 3 subset sum
+pub mod d02;  // password validation rules
+pub mod d03;  // toboggan tree trajectory
+pub mod d04;  // Passport validation
+pub mod d05;  // binary boarding
+pub mod d06;  // customs forms questions
+pub mod d07;  // bags of bags
+pub mod d08;  // handheld game CPU emulator
+pub mod d09;  // XMAS encryption
+pub mod d10;  // chaining adapter joltages
+// pub mod d11;  // Conway 2d
+pub mod d12;  // Ship movement
+pub mod d13;  // shuttle timetables, CRT
+pub mod d14;  // bitmask
+pub mod d15;  // elf memory game
+// pub mod d16;  // ticket scan
+// pub mod d17;  // Conway 3d and 4d
+// pub mod d18;  // RPN switchup
+// pub mod d19;  // recursive rules
+// pub mod d20;  // image data
+// pub mod d21;  // allergens
+// pub mod d22;  // Combat!
+// pub mod d23;  // Cups
+pub mod d24;  // Conway 2d Hex
+pub mod d25;  // Encryption decode
 
 aoc_lib! { year = 2020 }
